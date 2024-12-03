4b12a367e54fd65f825c5db4702989a8a882f716
JeMeSuisPerdu <anislafrad@gmail.com>
2024-12-03 13:51:11 +0100

    Ajoutez des fichiers projet.

---
+++ MediaTekDocuments/controller/FrmMediatekController.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using MediaTekDocuments.model;
+using MediaTekDocuments.dal;
+
+namespace MediaTekDocuments.controller
+{
+    /// <summary>
+    /// Contrôleur lié à FrmMediatek
+    /// </summary>
+    class FrmMediatekController
+    {
+        /// <summary>
+        /// Objet d'accès aux données
+        /// </summary>
+        private readonly Access access;
+
+        /// <summary>
+        /// Récupération de l'instance unique d'accès aux données
+        /// </summary>
+        public FrmMediatekController()
+        {
+            access = Access.GetInstance();
+        }
+
+        /// <summary>
+        /// getter sur la liste des genres
+        /// </summary>
+        /// <returns>Liste d'objets Genre</returns>
+        public List<Categorie> GetAllGenres()
+        {
+            return access.GetAllGenres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des livres
+        /// </summary>
+        /// <returns>Liste d'objets Livre</returns>
+        public List<Livre> GetAllLivres()
+        {
+            return access.GetAllLivres();
+        }
+
+        /// <summary>
+        /// getter sur la liste des Dvd
+        /// </summary>
+        /// <returns>Liste d'objets dvd</returns>
+        public List<Dvd> GetAllDvd()
+        {
+            return access.GetAllDvd();
+        }
+
+        /// <summary>
+        /// getter sur la liste des revues
+        /// </summary>
+        /// <returns>Liste d'objets Revue</returns>
+        public List<Revue> GetAllRevues()
+        {
+            return access.GetAllRevues();
+        }
+
+        /// <summary>
+        /// Getter sur les suivis
+        /// </summary>
+        /// <returns></returns>
+        public List<Suivi> GetAllSuivis()
+        {
+            return access.GetAllSuivis();
+        }
+
+        /// <summary>
+        /// Getter sur les etats
+        /// </summary>
+        /// <returns></returns>
+        public List<Etat> GetAllEtats()
+        {
+            return access.GetAllEtats();
+        }
+
+        /// <summary>
+        /// getter sur les rayons
+        /// </summary>
+        /// <returns>Liste d'objets Rayon</returns>
+        public List<Categorie> GetAllRayons()
+        {
+            return access.GetAllRayons();
+        }
+
+        /// <summary>
+        /// getter sur les publics
+        /// </summary>
+        /// <returns>Liste d'objets Public</returns>
+        public List<Categorie> GetAllPublics()
+        {
+            return access.GetAllPublics();
+        }
+
+
+        /// <summary>
+        /// récupère les exemplaires d'une revue
+        /// </summary>
+        /// <param name="idDocuement">id de la revue concernée</param>
+        /// <returns>Liste d'objets Exemplaire</returns>
+        public List<Exemplaire> GetExemplairesRevue(string idDocuement)
+        {
+            return access.GetExemplairesRevue(idDocuement);
+        }
+
+        /// <summary>
+        /// Crée un exemplaire d'une revue dans la bdd
+        /// </summary>
+        /// <param name="exemplaire">L'objet Exemplaire concerné</param>
+        /// <returns>True si la création a pu se faire</returns>
+        public bool CreerExemplaire(Exemplaire exemplaire)
+        {
+            return access.CreerExemplaire(exemplaire);
+        }
+
+        #region Commandes de livres et Dvd
+        /// <summary>
+        /// Récupère les commandes d'une livre
+        /// </summary>
+        /// <param name="idLivre">id du livre concernée</param>
+        /// <returns></returns>
+        public List<CommandeDocument> GetCommandesLivres(string idLivre)
+        {
+            return access.GetCommandesLivres(idLivre);
+        }
+
+        /// <summary>
+        /// Retourne l'id max des commandes
+        /// </summary>
+        /// <returns></returns>
+        public string GetNbCommandeMax()
+        {
+            return access.GetMaxIndex("maxcommande");
+        }
+
+        /// <summary>
+        /// Retourne l'id max des livres
+        /// </summary>
+        /// <returns></returns>
+        public string GetNbLivreMax()
+        {
+            return access.GetMaxIndex("maxlivre");
+        }
+
+        /// <summary>
+        /// Retourne l'id max des Dvd
+        /// </summary>
+        /// <returns></returns>
+        public string GetNbDvdMax()
+        {
+            return access.GetMaxIndex("maxdvd");
+        }
+
+        /// <summary>
+        /// Retourne l'id max des revues
+        /// </summary>
+        /// <returns></returns>
+        public string GetNbRevueMax()
+        {
+            return access.GetMaxIndex("maxrevue");
+        }
+
+        #endregion
+    }
+}
